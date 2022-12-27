ef2b5edf7e467ab02a51ce31bbd99007bfb7e22a
Fernando Javier Tejerina <101528363+fertejerina@users.noreply.github.com>
2022-12-27 20:07:54 -0300

    Cree la navbar con su respectiva hoja de estilos

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import './NavBar.css'
 
 const NavBar = () => {
+  const fecha = new Date()
+  console.log()
   return (
-  <div>asd</div>
+  <header className='navbar'>
+    <h1 className='title'>CHERRY</h1>
+    <p className='sub-title'>Primer pre-entrega</p>
+    <p>La Fecha de hoy es: "{fecha.toDateString()}"</p>
+    <ul className='links'>
+      <li><a href="">Inicio</a></li>
+      <li><a href="">Productos</a></li>
+      <li><a href="">Nosotros</a></li>
+      <li><a href="">Carrito</a></li>
+    </ul>
+  </header>
   )
 }
 
