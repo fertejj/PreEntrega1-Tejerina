0ecda65c40bd81ab372f9bfa5af01d57517fd1eb
fertejj <fertejerina97@gmail.com>
2023-01-30 09:58:39 -0300

    m

--- src/components/itemDetail/ItemDetail.jsx
+++ src/components/itemDetail/ItemDetail.jsx
@@ -5,9 +5,21 @@ import "./ItemDetail.css";
 
 const ItemDetail = (props) => {
 
-
+  const [cantidadDeProductosAComprar, setCantidadDeProductosAComprar] = useState(0);
   const { title, description, category, price, rating, image } = props.data;
 
+  
+  useEffect(()=>{
+    console.log(`estamos en itemDetail y el contador es ${cantidadDeProductosAComprar}, el total de su compra es = $${cantidadDeProductosAComprar*price}`);
+  //eslint-disable-next-line
+  },[cantidadDeProductosAComprar])
+  
+  const guardarCantidadBuy = (cantidadX) => {
+    setCantidadDeProductosAComprar(cantidadX)
+  }
+  function onAdd() {
+    console.log(cantidadDeProductosAComprar);
+  }
 
   return (
     <article className="itemDetail-producto">
@@ -20,7 +32,10 @@ const ItemDetail = (props) => {
       <p className="itemDetail-category">Categoria: {category}</p>
       <p className="itemDetail-descr">Descripcion: {description}</p>
       <p className="itemDetail-price">Precio: ${price}</p>
-      <ItemCount className="itemDetail-count" stock={rating.count} />
+      <ItemCount className="itemDetail-count" stock={rating.count} guardarCantidadBuy={guardarCantidadBuy}/>
+      <button className="add-cart" onClick={onAdd}>
+        Agregar al carrito
+      </button>
       <Link className='volver-productos' to="/productos"> Volver a la lista de productos</Link>
     </article>
   );
